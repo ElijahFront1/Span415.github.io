9f0b922958e15fd0fc2a58a173f52e760914ace5
Span415 <64959991+Span415@users.noreply.github.com>
2020-12-02 16:46:23 +0300

    adaptive changes5

--- js/accordin.js
+++ js/accordin.js
@@ -1,17 +1,20 @@
-const accordionBtn = document.querySelectorAll('.accordion');
-accordionBtn.forEach(btn => {
-  const panel = btn.nextElementSibling;
-  if(btn.classList.contains('always-active-accordion')) {
-    panel.style.maxHeight = '170px';
-    btn.classList.toggle('active');
-  }
-  btn.addEventListener('click', event => {
-    btn.classList.toggle('active');
-    if (btn.classList.contains('active')) {
-      panel.style.maxHeight = panel.scrollHeight = '170px';
+window.onload = () => {
+  const accordionBtn = document.querySelectorAll('.accordion');
+  accordionBtn.forEach(btn => {
+    const panel = btn.nextElementSibling;
+    console.log(panel.scrollHeight);
+    if (btn.classList.contains('always-active-accordion')) {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+      btn.classList.toggle('active');
     }
-    else {
-      panel.style.maxHeight = 0;
-    }
+    btn.addEventListener('click', event => {
+      btn.classList.toggle('active');
+      if (btn.classList.contains('active')) {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+      else {
+        panel.style.maxHeight = 0;
+      }
+    })
   })
-})
+}
